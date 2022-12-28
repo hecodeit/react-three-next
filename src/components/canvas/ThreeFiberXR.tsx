4fcf833744421074678817318bcf21b449914765
hecodeit <hecodeit@gmail.com>
2022-12-28 06:34:38 +0100

    use Grid as VR floor

--- src/components/canvas/ThreeFiberXR.tsx
+++ src/components/canvas/ThreeFiberXR.tsx
@@ -1,9 +1,7 @@
 import { Canvas } from '@react-three/fiber'
 import styles from '@/styles/ThreeFiber.module.css'
 import { VRButton, XR, Controllers, Hands } from '@react-three/xr'
-import { Sky } from '@react-three/drei'
-import Terrain from './Terrain'
-
+import { Grid, OrbitControls, Sky } from '@react-three/drei'
 
 export default function ThreeFiberXR({ children, ...props }) {
     // Everything defined in here will persist between route changes, only children are swapped
@@ -15,10 +13,11 @@ export default function ThreeFiberXR({ children, ...props }) {
                     <Controllers />
                     <Hands />
                     <Sky sunPosition={[0, 1, 0]} />
-                    <Terrain />
+                    <Grid args={[10, 10]} cellSize={.5} cellColor='#abbec7' cellThickness={1} sectionColor='#abbec7' sectionThickness={1} infiniteGrid fadeDistance={25} />
                     <directionalLight intensity={0.75} />
                     <ambientLight intensity={0.75} />
                     {children}
+                    <OrbitControls />
                 </XR>
             </Canvas>
         </div>
